7b149ea50b9f706738f650dd881c112fa11c66e9
Sol <99665938+keziavitorio@users.noreply.github.com>
2023-12-03 15:48:54 -0300

    finalização do projeto de estudo EJS

--- server.js
+++ server.js
@@ -4,11 +4,40 @@ const app = express()
 app.set("view engine", "ejs") // ferramente de renderizar HTMl é o EJS
 
 app.get("/", function (req, res) {
-    res.render("index")
+    const items = [{
+            title: "D",
+            message: "esenvolver aplicações/serviços de forma fácil"
+        },
+        {
+            title: "E",
+            message: "JS usa Javascript para renderizar HTML"
+        },
+        {
+            title: "M",
+            message: "uito fácil de usar"
+        },
+        {
+            title: "A",
+            message: "morzinho"
+        },
+        {
+            title: "I",
+            message: "nstall EJS"
+        },
+        {
+            title: "S",
+            message: "intaxe simples"
+        }
+    ]
+    const subtittle = "Uma lingaguem de modelagem para criação de paginas HTML utilizando Javascript."
+    res.render("pages/index", {
+        qualitys: items,
+        subtittle: subtittle,
+    })
 })
 
 app.get("/sobre", function (req, res) {
-    res.render("about")
+    res.render("pages/about")
 })
 
 app.listen(8080)
